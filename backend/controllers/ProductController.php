f6040c43af8457d426c342e78e44d1f07424c7bb
beoipisilon <48164586+beoipisilon@users.noreply.github.com>
2025-04-06 21:23:14 -0300

    try to fix cors

--- backend/controllers/ProductController.php
+++ backend/controllers/ProductController.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../repositories/ProductRepository.php';
+require_once __DIR__ . '/../config/cors.php';
 
 class ProductController {
     private $productRepository;
@@ -9,15 +10,9 @@ class ProductController {
     }
 
     public function handleRequest() {
+        handleCORS();
+        
         header('Content-Type: application/json; charset=utf-8');
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type');
-
-        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            http_response_code(200);
-            exit();
-        }
 
         try {
             $searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
